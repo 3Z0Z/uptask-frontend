b569481b60efd46f771218072591d8ebf1669b38
ezoz <ezozplayer11@gmail.com>
2024-12-10 09:19:21 -0500

    first commit

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "@/layouts/AppLayout";
+import DashboardPage from "@/pages/DashboardPage";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout/>}>
+          <Route path="/" element={<DashboardPage/>} index />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
